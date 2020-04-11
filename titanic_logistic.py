71aa3555787f8406ca4287b304f09cc6ed374cfd
mvb <oiramxd@gmail.com>
2020-04-10 22:24:06 -0500

    Changes to be committed: 	new file:   aaron_judge.py 	new file:   david_ortiz.py 	new file:   jose_altuve.py 	modified:   titanic_logistic.py

--- titanic_logistic.py
+++ titanic_logistic.py
@@ -5,62 +5,66 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-# Load the passenger data
+# Cargando la base de datos de los pasajeros
 passengers = pd.read_csv("passengers.csv")
 
+#print(passengers.columns)
+print(passengers.head())
 
-# Update sex column to numerical
+######### Limpiando la base de datos
 passengers['Sex'] = passengers['Sex'].apply(lambda x: 1 if x=='female' else 0)
 
-# Fill the nan values in the age column
+# Eliminando NaN por la edad promedio
 mean_age = passengers['Age'].mean(axis=0)
 passengers['Age'].fillna(mean_age,inplace = True)
 #print(passengers['Age'].values)
 
-# Create a first class column
+# Reorganizando datos...
 passengers['FirstClass'] = passengers['Pclass'].apply(lambda x: 1 if x ==1 else 0)
 
-# Create a second class column
+# Second class column
 passengers['SecondClass'] = passengers['Pclass'].apply(lambda x: 1 if x ==2 else 0)
-print(passengers)
+print(passengers.head())
 
-# Select the desired features
+# Características importantes para hacer la regresión
 features = passengers[['Sex','Age','FirstClass','SecondClass']]
 survival = passengers['Survived']
 
-# Perform train, test, split
+# Separando datos de prueba y entrenamiento
 f_train, f_test, s_train,s_test = train_test_split(features,survival,test_size=0.2,random_state = 100)
 print(f_test.shape,f_train.shape)
-# Scale the feature data so it has mean = 0 and standard deviation = 1
+# Reescalando los datos para no tener problemas de escala media = 0 y standard deviation = 1
 scaler = StandardScaler()
 f_train = scaler.fit_transform(f_train)
 f_test = scaler.transform(f_test)
 
-# Create and train the model
+###### Regresión Logistica
 model = LogisticRegression()
 model.fit(f_train,s_train)
 
-# Score the model on the train data
+# Precisión de la data de entrenamiento
 print(model.score(f_train,s_train))
 
-# Score the model on the test data
+# Precisión de la data de prueba
 print(model.score(f_test,s_test))
 
-# Analyze the coefficients
+# Coeficientes de la regresión.
+### Coeficientes << 1 => mayor probabilidad clase positiva
+### Coeficientes >> -1 => mayor probabilidad clase negativa|
 print(model.coef_)
 
-# Sample passenger features
+#### Haciendo predicciones
 Jack = np.array([0.0,20.0,0.0,0.0])
 Rose = np.array([1.0,17.0,1.0,0.0])
 me = np.array([0.,5.,1.,0.])
 
-# Combine passenger arrays
+# Combinando arreglos
 sample_passengers = np.array([Jack,Rose,me])
 
-# Scale the sample passenger features
+# Reescalando
 sample_passengers = scaler.transform(sample_passengers)
 
-# Make survival predictions!
+# Prediciendo !
 print(model.predict(sample_passengers))
 print(model.predict_proba(sample_passengers))
 
